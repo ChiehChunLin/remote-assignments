c2c69c497abb4c494867db78fe7aa3c5c6e71f3b
Cherry Lin <sukusukustudio@protonmail.com>
2024-04-10 21:17:07 +0800

    week-3 assignments restruct domHandler as a new file

---
+++ Week-3/Assignments/public/domHandler.js
@@ -0,0 +1,76 @@
+//-------------------------------------
+//------   DOM Event Handler ----------
+//--------------------------------------
+document.addEventListener("DOMContentLoaded", function () {
+  $(document).ready(function () {
+    $("form.mySumForm").submit(calculation);
+    $("form.myNameForm").submit(setMyName);
+
+    function setMyName(e) {
+      e.preventDefault();
+      const newName = $("#myName").val();
+      const url = `http://localhost:3000/trackName?name=${newName}`;
+      console.log("fetch:" + url);
+
+      const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      fetch(url, config)
+        .then(checkStatus)
+        .then((res) => {
+          if (res.redirected) {
+            location.href = res.url;
+            return;
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          if (data) {
+            const { message } = data;
+            $(".msgBox").text(message);
+          }
+        })
+        .catch((err) => {
+          $(".msgBox").text(err);
+          console.error("fetch error:", err);
+        });
+    }
+    function calculation(e) {
+      e.preventDefault();
+      const number = $("#myNumber").val();
+      const url = `http://localhost:3000/data?number=${number}`;
+      console.log("fetch:" + url);
+
+      const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      fetch(url, config)
+        .then(checkStatus)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const { message } = data;
+          $(".msgBox").text(message);
+        })
+        .catch((err) => {
+          console.error("fetch error:", err);
+        });
+    }
+    function checkStatus(response) {
+      if (response.ok) {
+        return Promise.resolve(response);
+      } else {
+        return Promise.reject(new Error(response.statusText));
+      }
+    }
+  });
+});
